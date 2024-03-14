ff00396644321a5687c3593d295eca1f22f54ca0
JelenaRjazanska1972 <160174934+JelenaRjazanska1972@users.noreply.github.com>
2024-03-14 20:17:58 +0100

    #4 file SortPhons was created

---
+++ src/main/java/ait/game/phones/sort/SortPhons.java
@@ -0,0 +1,13 @@
+package ait.game.phones.sort;
+
+public class SortPhons {
+    public static void main(String[] args) {
+
+    }
+
+    public static void sortPhonesByPrice (CellPhone [] phons){
+
+        for (CellPhone phon: phons) {
+
+    }
+}
