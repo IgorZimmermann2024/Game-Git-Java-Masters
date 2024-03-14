8821ea34f1e4fb8288a39b47dacd379560f1f965
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-14 21:08:42 +0100

    #3 filter phones

--- src/main/java/ait/game/phones/filter/FilterPhone.java
+++ src/main/java/ait/game/phones/filter/FilterPhone.java
@@ -1,35 +1,42 @@
 package ait.game.phones.filter;
 import java.util.Arrays;
+import ait.game.phones.classCellPhone.CellPhone;
 
-public class CellPhone {
+public class FilterPhone {
     /*
     ### Задача 3: Фильтрация телефонов по ценовому диапазону
 
 **Цель:** Создать функцию, которая фильтрует массив телефонов по заданному ценовому диапазону.
 
 **Описание задачи:**
-- На основе класса `CellPhone` и массива телефонов реализуйте функцию `filterPhonesByPriceRange`,
+- На основе класса `` и массива телефонов реализуйте функцию `filterPhonesByPriceRang`,
 которая принимает два аргумента: минимальную и максимальную цену.
 - Функция должна возвращать новый массив, содержащий только те телефоны,
 цена которых находится в указанном диапазоне.
 
      */
-    String brand;
-    String model;
-    double price;
+    public static CellPhone[] filterPhonesByPriceRange(double minPrice, double maxPrice, CellPhone[] phones) {
+        filteredPhones = filterPhonesByPriceRange(phones,minPrice,maxPrice);
 
-    public CellPhone(String brand,String model,double price){
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
+        filteredPhones = new ArrayList<>();
+        for (CellPhone phone : phones){
+            double price = phone.getPrice();
+            if (price >= minPrice && price <= maxPrice){
+                filteredPhones.add(phone);
+
+            }
+
+        }
+        return filteredPhones;
+
+
+
+
+
     }
-    public double getPrice(){
-        return price;
-    }
 
 
-
-    }
+}
 
 
 
