2fdcaa17d32f5bd9a9c8ce01106c840cbaa93045
JelenaRjazanska1972 <160174934+JelenaRjazanska1972@users.noreply.github.com>
2024-03-14 21:15:54 +0100

    #4 save result sortPhonesByPrice

--- src/main/java/ait/game/phones/sort/SortPhons.java
+++ src/main/java/ait/game/phones/sort/SortPhons.java
@@ -1,13 +1,14 @@
 package ait.game.phones.sort;
 
-import ait.game.phones.classCellPhone.CellPhone;
+
+import ait.game.phones.phone.CellPhone;
 
 public class SortPhons {
     public static void main(String[] args) {
-
+CellPhone cellPhone=new CellPhone();
     }
 
-    public static void sortPhonesByPrice (CellPhone [] phons){
+    public static void sortPhonesByPrice (CellPhone[] phons){
 
         for (CellPhone phon: phons) {
             System.out.println(phon.toString());
